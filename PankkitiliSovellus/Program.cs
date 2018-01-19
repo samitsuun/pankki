fe81c9905a34a5cbb6715b2dcb4daf9f06241eba
it15 <it15@labra.rmk.hamk.fi>
2018-01-19 11:15:47 +0200

    lisataan ominaisuuksia

--- PankkitiliSovellus/Program.cs
+++ PankkitiliSovellus/Program.cs
@@ -11,6 +11,14 @@ namespace PankkitiliSovellus
         static void Main(string[] args)
         {
             Console.WriteLine("PankkitiliSovellus v1");
+
+            Console.WriteLine("Anna alkusaldon määrä: ");
+
+            string alkusaldo = Console.ReadLine();
+
+            Pankkitili pankkitili1 = new Pankkitili(int.Parse(alkusaldo));
+
+            Console.WriteLine("Tilin 01 alkusaldo on: " + pankkitili1.Saldo);
         }
     }
 }
