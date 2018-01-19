336794654cf80a6a956ac099fac9f4c7151dae48
it15 <it15@labra.rmk.hamk.fi>
2018-01-19 11:30:05 +0200

    muutoksia lisaa

--- PankkitiliSovellus/Program.cs
+++ PankkitiliSovellus/Program.cs
@@ -13,13 +13,23 @@ namespace PankkitiliSovellus
             Console.WriteLine("PankkitiliSovellus v1");
 
             Console.WriteLine("Anna alkusaldon määrä: ");
-
             string alkusaldo = Console.ReadLine();
 
             Pankkitili pankkitili1 = new Pankkitili(int.Parse(alkusaldo));
-
             Console.WriteLine("Tilin 01 alkusaldo on: " + pankkitili1.Saldo);
 
+            Console.WriteLine("Anna nostettava rahamäärä: ");
+            String rahamaara = Console.ReadLine();
+
+            try
+            {
+                pankkitili1.NostaRahaa(int.Parse(rahamaara));
+                Console.WriteLine("Tilin 01 saldo on " + pankkitili1.Saldo);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Tilillä 01 ei riitä nostovarat.");
+            }
 
         }
     }
