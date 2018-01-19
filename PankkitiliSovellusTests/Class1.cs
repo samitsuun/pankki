ff03498ebc4e8c14f7d3cf83fc1b908d3a4aee8f
it15 <it15@labra.rmk.hamk.fi>
2018-01-19 09:15:52 +0200

    Ensimmainen testaus

--- PankkitiliSovellusTests/Class1.cs
+++ PankkitiliSovellusTests/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace PankkitiliSovellusTests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void EnsimmainenTesti ()
+        {
+            Assert.That(2, Is.EqualTo(2));
+        }
     }
 }
